7c39067c2fe469fd0b2435828eabe51f3f1aada7
Vaishali913 <105474022+Vaishali913@users.noreply.github.com>
2023-04-16 07:41:05 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,11 @@
+function validate(){
+    var userid = document.getElementById("userid").value;
+    var password = document.getElementById("password").value;
+    if (userid == "ritik9428" && password == "NKNH3@123"){
+        alert("Login Successfully");
+        return true;
+    }
+    else{
+        
+    }
+}
